2eaebec7f1d5e48161b8d2bad053d0eb36ae0285
kemo12 <dodkamel919@gmail.com>
2020-10-03 14:08:35 +0300

    dom updates 2

--- js/dom.js
+++ js/dom.js
@@ -1,15 +1,34 @@
+//setting the tab mode 
+//in this file i use events of button to swap between stopwatch and timer
 const stopWatchButton = document.querySelector(".stopWatchButton");
 const timerTabButton=document.querySelector('.timerTabButton');
 
-stopWatchButton.addEventListener('click',()=>{
-    document.querySelector('.stop-watch-tab').classList.add('active');
-    document.querySelector('.timer-tab').classList.remove('active');
-    document.querySelector('.stopWatchButton').classList.add('active-btn');
-    document.querySelector('.timerTabButton').classList.remove('active-btn');
-});
 timerTabButton.addEventListener('click',()=>{
 document.querySelector('.timer-tab').classList.add('active');
 document.querySelector('.stop-watch-tab').classList.remove('active');
 document.querySelector('.timerTabButton').classList.add('active-btn');
 document.querySelector('.stopWatchButton').classList.remove('active-btn');
+document.querySelector('.timer-buttons-container').classList.add('active');
+document.querySelector('.stopwatch-buttons-container').classList.remove('active');
+});
+stopWatchButton.addEventListener('click',()=>{
+    document.querySelector('.stop-watch-tab').classList.add('active');
+    document.querySelector('.timer-tab').classList.remove('active');
+    document.querySelector('.stopWatchButton').classList.add('active-btn');
+    document.querySelector('.timerTabButton').classList.remove('active-btn');
+    document.querySelector('.stopwatch-buttons-container').classList.add('active');
+document.querySelector('.timer-buttons-container').classList.remove('active');
+});
+document.querySelector('.start-stopwatch').addEventListener('click',()=>{
+    document.querySelector('.stop-stopwatch').classList.remove('displynone');
+    document.querySelector('.reset-stopwatch').classList.remove('displynone');
+    document.querySelector('.start-stopwatch').classList.add('displynone');
+    
+
+});
+document.querySelector('.stop-stopwatch').addEventListener('click',()=>{
+    document.querySelector('.start-stopwatch').classList.remove('displynone');
+    document.querySelector('.reset-stopwatch').classList.remove('displynone');
+    document.querySelector('.stop-stopwatch').classList.add('displynone');
+
 });
